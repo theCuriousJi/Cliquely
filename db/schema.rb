94393339f2523f12ed672d84fcbf03d6ba250e36
Gaurav Nagpal <gauravnagpal19@gmail.com>
2015-02-03 15:25:01 -0800

    Switched db to PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150203192440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "fname",           null: false
     t.string   "lname",           null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150203192440) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
 end
